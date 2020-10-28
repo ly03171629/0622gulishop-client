939301abf721962f9d682e719170091bd148218f
liuyuan <1316425655@qq.com>
2020-10-28 17:13:28 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <Header></Header>
+    <router-view></router-view>
+    <!-- <Footer v-show="$route.path !== '/login' && $route.path !== '/register'"></Footer> -->
+    <Footer v-show="!$route.meta.isHidden"></Footer>
+  </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default {
-  name: '',
+  name: 'App',
+  components:{
+    Header,
+    Footer
+  }
 }
 </script>
 
